5dd2039a759768c44f844622457694fab5ab67b6
aaiden Nicolas Witten <aaidenplays@yahoo.com>
2020-01-02 16:19:52 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
@@ -13,18 +14,18 @@
 #     }
 #   }
 # }
-# 
+
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
@@ -32,14 +33,34 @@
 
 def naming_system
   # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
+  #   Kingdom
+  #   Phylum
+  #   Class
+  #   Order
+  #   Family
+  #   Genus
+  #   Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+  
+  species = {  
+    sub_category: { 
+      sub_category: {
+        sub_category: {
+          label: "Order",
+          sub_category: { 
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+              }
+  
+  #binding.pry
+  species
 end
