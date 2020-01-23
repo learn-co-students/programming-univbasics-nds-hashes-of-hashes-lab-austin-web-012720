1f0ec3167341df0622d594a5afe06f084cfbb9dc
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-23 01:51:28 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,18 +13,18 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
@@ -42,4 +42,22 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
+  species = {
+    sub_category: {
+      sub_category: {
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            sub_category:{
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  species
 end
